11025d743ab6c4dd16795e6c997feabe2b751e1b
Pavelsphone <64527633+Pavelsphone@users.noreply.github.com>
2024-10-07 19:24:04 +0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int arrayList[] = {3,5,7,1,8,95,32,76,98,31,17};
+        System.out.println("Длина массива arrayList равна "+arrayList.length);
+    }
+}
